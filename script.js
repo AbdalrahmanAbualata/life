60fa7b4d67ca86cf8642240c842d0642939425eb
abdata <>
2021-06-03 11:47:36 +0000

    script

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+ var userName = prompt("Enter your Name Plz ? ");
+console.log(userName);
+alert(" hello (( "+ userName +" )) welcome to our Website ")
+
+document.write("<p style='color:#fff5fd'> *** good bye ( " +userName+ " )*** </p>")
+
+var FavoriteSport = prompt("choose number for  your favorite sport : 1-football , 2-basketball , 3-bowling  ? ");
+console.log(FavoriteSport);
+
+ if (FavoriteSport == 1) {
+  alert("you are the best")}
+  else if (FavoriteSport == 2) {
+  alert(" no bad sport ;-)")
+} else {
+  alert("enjoy your stay")
+}
+
+// end
+// bye bye ;) 
+
